6ec2c666c8ffc78e8c56aa77ccff50d9b1e269f1
Alex <47276653+JShurik@users.noreply.github.com>
2022-03-09 23:25:02 +0300

    Add files via upload

--- second_sem/poly.h
+++ second_sem/poly.h
@@ -1,14 +1,25 @@
 #pragma once
 
+
+enum expt { E0, E1, E2, E3, E4 };
+
+typedef struct exception {
+    int pos;
+    expt e;
+} exeption;
+
 typedef struct poly {
     int coeff;
     int exp;
     struct poly* next;
 } poly;
 
-poly* poly_get(const char* input);
-void poly_free(poly *p);
-void poly_print(poly *p);
+void poly_free(poly* p);
+void print_exeption(exeption* ex);
+poly* poly_get_monomial(int coeff, int exp);
 
+void poly_merge(poly** p1, poly* p2);
+poly* poly_get(const char* input);
+void poly_print(poly* p);
 poly* poly_add(poly* p1, poly* p2);
 poly* poly_multy(poly* p1, poly* p2);
