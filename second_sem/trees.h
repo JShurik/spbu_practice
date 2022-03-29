1bba7da1bdc4242a96dbd640c0b7b2577d53c1b8
Alex <47276653+JShurik@users.noreply.github.com>
2022-03-29 21:04:33 +0300

    updating forth task, unknown errors

--- second_sem/trees.h
+++ second_sem/trees.h
@@ -2,11 +2,18 @@
 
 typedef struct tree_node {
 	int data;
-	tnode* left;
-	tnode* right;
+	struct tree_node* left;
+	struct tree_node* right;
 } tnode;
 
-void tree_print(tnode *tree);
+void tree_print(tnode* tree);
 void tree_free(tnode** tree);
 void add_node(tnode** tree, int node);
 tnode* tree_sort(const char* str);
+
+typedef struct sb_tree {
+	int a;
+	int b;
+	struct sb_tree* left;
+	struct sb_tree* right;
+} sbt;
